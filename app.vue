86793d0964380d3d85d416c448f82206f6838c93
perplexingcode <buisondong20@gmail.com>
2023-12-01 05:39:52 +0700

    Add temp favicon

--- app.vue
+++ app.vue
@@ -5,3 +5,10 @@
     </Data>
   </div>
 </template>
+<script setup>
+// Add favicon and title
+useHead({
+  title: 'Trinity Inc - We build a better world',
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
+});
+</script>
