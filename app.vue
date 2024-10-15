fbd985dc5af62203328edfffd5fcff4d0d52b66d
perplexingcode <buisondong20@gmail.com>
2024-10-15 20:35:09 +0700

    quick-save

--- app.vue
+++ app.vue
@@ -6,9 +6,20 @@
   </div>
 </template>
 <script setup>
-// Add favicon and title
+const trinityAI = {
+  src: 'https://mindmaid.ai/embed-assistant.js" data-chat-service="Salebot" data-bot-id="bd409e68-7395-49d2-9fd1-36b6bee222d7',
+  'data-chat-service': 'Salebot',
+  'data-bot-id': 'bd409e68-7395-49d2-9fd1-36b6bee222d7',
+  'data-chat-width': '450px',
+  'data-chat-height': '600px',
+  'data-bubble-color': '',
+  async: true,
+  defer: true,
+  crossorigin: 'anonymous',
+};
 useHead({
   title: 'Trinity Inc - We build a better world',
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
+  script: [trinityAI],
 });
 </script>
