2777909678299c634f1fba9099304edf594d6add
perplexingcode <buisondong20@gmail.com>
2024-11-06 17:48:00 +0700

    quick-save

--- app.vue
+++ app.vue
@@ -9,7 +9,7 @@
 const trinityAI = {
   src: 'https://mindmaid.ai/embed-assistant.js',
   'data-chat-service': 'Salebot',
-  'data-bot-id': 'bd409e68-7395-49d2-9fd1-36b6bee222d7',
+  'data-bot-id': '1898c888-29ae-4fca-b7db-e5f842848eb7',
   'data-chat-width': '450px',
   'data-chat-height': '600px',
   'data-bubble-color': '',
